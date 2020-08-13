99d1c1a318fbc510035128c0a7ab6c0e5b8d1e2d
MasonM <Mason.P.Marinko@gmail.com>
2020-08-13 11:32:59 -0600

    working for presentation

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 const bookRoutes = require('./book-routes');
+const bookCommentRoutes=require('./book-comment-routes')
 
 router.use('/users', userRoutes);
 router.use('/movies', postRoutes);
-router.use('/comments', commentRoutes);
+router.use('/movie/comments', commentRoutes);
+router.use('/book/bookcomments', bookCommentRoutes)
 router.use('/books', bookRoutes);
 
 module.exports = router;
--- public/javascript/book-comment.js
+++ public/javascript/book-comment.js
@@ -8,7 +8,7 @@ async function commentFormHandler(event) {
     ];
   
     if (comment_text) {
-        const response = await fetch('/api/comments', {
+        const response = await fetch('/api/book/comments', {
           method: 'POST',
           body: JSON.stringify({
             post_id,
