534d5c6ea984b8f8916dfd8223f37de23c7b072b
MasonM <Mason.P.Marinko@gmail.com>
2020-08-13 00:14:05 -0600

    pushing for BCS to review

--- public/javascript/book-comment.js
+++ public/javascript/book-comment.js
@@ -1,7 +1,7 @@
 async function commentFormHandler(event) {
     event.preventDefault();
   
-    const comment_text = document.querySelector('textarea[name="movie-comment-body"]').value.trim();
+    const comment_text = document.querySelector('textarea[name="book-comment-body"]').value.trim();
   
     const post_id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
@@ -27,4 +27,4 @@ async function commentFormHandler(event) {
       }
   }
   
-  document.querySelector('.movie-comment-form').addEventListener('submit', commentFormHandler);
+  document.querySelector('.book-comment-form').addEventListener('submit', commentFormHandler);
