2b8a3bf6354b9ac56ce446a6ba18507c1b81d3f0
MasonM <Mason.P.Marinko@gmail.com>
2020-08-16 15:05:38 -0600

    fixed login screen

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,7 +1,7 @@
 async function loginFormHandler(event) {
     event.preventDefault();
     const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const password = document.querySelector('#password').value.trim();
 
     if (username && password) {
         const response = await fetch('/api/users/login', {
